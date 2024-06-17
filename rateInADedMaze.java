6119e2780236aa76e8ce102302f9733d27d7893d
cyCaffine <14111menkashah@gmail.com>
2024-06-18 01:55:17 +0530

    Add files via upload

---
+++ rateInADedMaze.java
@@ -0,0 +1,30 @@
+public class rateInADedMaze {
+    public static void main(String[] args) {
+        
+    
+    int col = 4;
+    int row = 6;
+    int [][] maze = {
+        {1,0,1,1,1,1},
+        {1,1,1,1,0,1},
+        {0,1,1,1,1,1},
+        {0,0,1,0,1,1} };
+     printmaze(0, 0, row-1,col-1, " ",maze);
+}
+
+private static void printmaze(int sr, int sc, int er, int ec, String s, int maze[][]) {
+
+    if(sr > er || sc > ec) return ;
+    if(sr == er && sc == ec){
+        System.out.println(s);
+        return;
+    }
+    if(maze[sr][sc] == 0)return;
+    // go down
+    printmaze(sr+1, sc, er, ec, s+"D",maze);
+    // go right
+    printmaze(sr, sc+1, er, ec, s+"R",maze);
+
+}
+}
+
